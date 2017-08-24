187ff2f993806945b94c5f4d5bea81088d9984de
Nick Sillik <nick@sillik.org>
2017-08-24 15:16:19 -0700

    Working ios splash screen example

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Just a test app to test out then new iOS splash screen</Text>
       </View>
     );
   }
